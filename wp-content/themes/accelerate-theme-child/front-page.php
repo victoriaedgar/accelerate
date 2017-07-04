bf77c18d5b190e3fb7ff07e781301980f9bc9069
victoriaedgar <victoria.edgar524@gmail.com>
2017-07-03 20:07:26 -0400

    first commit

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -14,7 +14,6 @@
 
 get_header(); ?>
 
-<pre><?php print_r($wp_query); exit; ?></pre>
 
 <section class="home-page">
 	<div class="site-content">
@@ -27,4 +26,18 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+            <section class="recent-posts">
+             <div class="site-content">
+              <div class="blog-post">
+               <h4>From the Blog</h4>
+                <?php query_posts('posts_per_page=1'); ?>
+                 <?php while ( have_posts() ) : the_post(); ?>
+                   <h2><?php the_title(); ?></h2>
+                   <?php the_excerpt(); ?> 
+                   <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+                 <?php endwhile; ?> 
+                <?php wp_reset_query(); ?>
+               </div>
+              </div>
+            </section>
 <?php get_footer(); ?>
